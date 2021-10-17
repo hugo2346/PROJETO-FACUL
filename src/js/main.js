911d894c7c1def43aa34a0e9d5f55739fb665376
Gabriel Tijon da Silva <75736869+TijonSilva@users.noreply.github.com>
2021-10-17 19:22:33 -0300

    carousel finalizado uhuu

--- src/js/main.js
+++ src/js/main.js
@@ -41,4 +41,29 @@ jQuery(document).ready(function($) {
         },
 
     });
+    //OWL CAROUSEL
+    $(document).ready(function() {
+        $(".owl-carousel").owlCarousel({
+            loop: false,
+            margin: 30,
+            autoplay: true,
+            autoplayTimeout: 2000,
+            dots: true,
+            lazyLoad: true,
+            nav: false,
+            responsive: {
+                0: {
+                    items: 1,
+                },
+                600: {
+                    items: 1,
+                },
+                1000: {
+                    items: 2,
+                },
+            },
+
+        });
+    });
+
 });
