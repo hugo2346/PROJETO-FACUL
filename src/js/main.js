b6b88afef6a647088af833287901cd469971601b
Gabriel Tijon da Silva <75736869+TijonSilva@users.noreply.github.com>
2021-10-17 15:39:44 -0300

    up

--- src/js/main.js
+++ src/js/main.js
@@ -8,5 +8,25 @@ jQuery(document).ready(function($) {
         } else {
             $("#header").removeClass("active")
         }
-    }
+    };
+
+    //ISOTOPE
+    let btns = $("#servicos .button-group button")
+
+    btns.click(function(e) {
+        $("#servicos .button-group button").removeClass("active")
+        e.target.classList.add("active")
+
+        let selector = $(e.target).attr("data-filter")
+        $("#servicos .grid").isotope({
+            filter: selector,
+        });
+    });
+
+    $(window).on("load", function() {
+        $("#servicos .grid").isotope({
+            filter: "*",
+        })
+    })
+
 })
